77c5d8c615bafc32c697caf2560c12629e36124a
thuyphanthanhpy <thuyphanthanhpy@gmail.com>
2024-07-13 10:43:33 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,6 +1,12 @@
+import './style.css';
 const MyComponent = ()=>{
     return (
-    <div> eric & hoidanit update</div>
+      <> 
+        <div> eric & hoi dan it update</div>
+        <div className= "child"
+          style={{borderRadius: "10px"}}
+        >child</div>
+      </>
     );
   }
   export default MyComponent
